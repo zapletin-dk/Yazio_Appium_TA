79a6570df6625af4ef8fd7c62ef3d7ebaae1c635
Daniil Zapletin <zapletin.dk@gmail.com>
2025-05-29 18:45:51 +0500

    Deleted unnecessary test

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -11,11 +11,4 @@ public class LoginPageTest extends BaseTest {
         stepDefinitions.loginAs(UserFactory.getExistingUser());
         Assert.assertTrue(stepDefinitions.isWelcomeBackTextVisibleAfterLogIn(), "Welcome back text isn't visible after login");
     }
-
-    @Test
-    @Description("Test to verify that the user can sign in with an existing account")
-    public void nInAsExistingUserTest() {
-        stepDefinitions.loginAs(UserFactory.getExistingUser());
-        Assert.assertTrue(stepDefinitions.isWelcomeBackTextVisibleAfterLogIn(), "Welcome back text isn't visible after login");
-    }
 }
